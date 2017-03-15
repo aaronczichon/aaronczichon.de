8203c64a63ff69d9263316debff50eb31f7c277c
Aaron Czichon <aaron@Aarons-MacBook-Pro.local>
2017-03-15 12:04:37 +0100

    added cloud functions samples

---
+++ IGWorkshops_cloudFunctions/functions/index.js
@@ -0,0 +1,56 @@
+let functions = require('firebase-functions');
+let admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.sendPush = functions.database.ref('/projects/${id}').onWrite(event => {
+    let projectStateChanged = false;
+    let projectCreated = false;
+    let projectData = event.data.val();
+    if (!event.data.previous.exists()) {
+        projectCreated = true;
+    }
+    if (!projectCreated && event.data.changed()) {
+        projectStateChanged = true;
+    }
+
+    let msg = 'A project state was changed';
+
+    if (projectCreated) {
+        msg = `The following new project was added to the project: ${projectData.title}`;
+    }
+
+    return loadUsers().then(users => {
+        let tokens = [];
+        for (let user of users) {
+            tokens.push(user.pushToken);
+        }
+
+        let payload = {
+            notification: {
+                title: 'Firebase Notification',
+                body: msg,
+                sound: 'default',
+                badge: '1'
+            }
+        };
+        
+        return admin.messaging().sendToDevice(tokens, payload);
+    });
+});
+
+function loadUsers() {
+    let dbRef = admin.database().ref('users/');
+    let defer = new Promise((resolve, reject) => {
+        dbRef.once('value', (snap) => {
+            let data = snap.val();
+            let users = [];
+            for (var property in data) {
+                users.push(data[property]);
+            }
+            resolve(users);
+        }, (err) => {
+            reject(err);
+        });
+    });
+    return defer;
+}
--- readme.md
+++ readme.md
@@ -6,3 +6,5 @@ This repository contains all projects which are related to articles on [aaronczi
 
 * [Ionic 2 and Firebase Authentication](https://aaronczichon.de/2017/03/07/ionic-firebase-authentication/)
     * [Sample Project](https://github.com/Inoverse/aaronczichon.de/tree/master/IGWorkshops)
+* [Sending FCM Push using Cloud Functions for Firebase](https://aaronczichon.de/2017/03/13/firebase-cloud-functions/)
+    * [Sample Project](https://github.com/Inoverse/aaronczichon.de/tree/master/IGWorkshops_cloudFunctions)
